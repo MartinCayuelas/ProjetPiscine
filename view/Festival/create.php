f8070b207bad654150fa5b9e93d3154411054dc5
MathiasPoulain <32484618+MathiasPoulain@users.noreply.github.com>
2018-01-01 16:48:44 +0100

    Add files via upload

---
+++ view/Festival/create.php
@@ -0,0 +1,41 @@
+
+<div class="card card-register mx-auto mt-5">
+    <div class="card-body">
+        <div class="card-header text-center"><?php echo $titre; ?> Festival</div>
+
+        <form method="post" action="index.php?action=<?php echo $action; ?>" >
+            <div class="form-group">
+                <div class="form-row">
+                    <div class="col-md-6">
+                        <label class="card-header" for="date_id">Date</label>
+                        <input type="text" value="<?php echo $date ?>"placeholder="Date" name="date" id="date_id"required/>
+                    </div>
+                 
+                      <div class="col-md-6">
+                            <label class="card-header" for="nbtables_id">NbreTables</label>
+                            <input type="text" value="<?php echo $nbtables ?>"placeholder="NbreTables" name="nbtables" id="nbtables_id"/>
+                        
+                       
+                            <label class="card-header" for="prixplacestd_id">PrixPlaceStandard</label>
+                            <input type="number" value="<?php echo $prixplacestd ?>"placeholder="PrixPlaceStandard" name="prixplacestd" id="prixplacestd_id"/>
+                        
+                        		</div>
+                    </div>
+                    </div>
+                </div>
+                
+            </div>
+
+
+            <div class="form-row">
+
+                <div class="col-lg-12 text-center"> 
+                    <input type="hidden" name="anneeFestival" value="<?php echo $num ?>" />
+                    
+                    <input class="btn btn-success" type="submit" value="Enregistrer" />
+                </div>
+            </div>
+    </div>
+</form>
+</div>
+
---
+++ view/Festival/list.php
@@ -0,0 +1,70 @@
+<?php
+echo <<< EOF
+ <div class="card mb-3">
+        <div class="card-header">
+          <i class="fa fa-table"></i> Liste des festivals</div>
+EOF;
+if (isset($_SESSION['login']) && Session::is_admin()) {
+    echo <<<EOF
+            <a class="ajout" href="index.php?action=createFestival">Ajouter un Festival</a>
+             
+EOF;
+}
+echo <<<EOF
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+               <thead>
+                <tr>
+                  
+                  
+                  <th>Date</th>
+                  <th>NbreTables</th>
+                   <th>PrixPlaceStandard</th>
+                </tr>
+              </thead>
+EOF;
+foreach ($tab as $v) {
+    $annee = htmlspecialchars($v->getAnneeFestival());
+    $date = htmlspecialchars($v->getDateFestival());
+    $nbtables = htmlspecialchars($v->getNbTablesFestival());
+    $prixplacestd = htmlspecialchars($v->getPrixPlaceStdFestival());
+    echo <<< EOF
+             <tbody>
+                <tr>
+                  
+                  <th>{$annee}</th>
+                   <th>{$date}</th>
+                   <th>{$nbtables}</th>
+                   <th>{$prixplacestd}</th>
+EOF;
+    if (isset($_SESSION['login']) && Session::is_admin()) {
+        echo <<< EOF
+                   
+                   <th class="text-center" ><a href="index.php?action=deleteFestival&annee={$annee}"><button class="btn btn-danger" type="button">Supprimer</button></a></th> 
+                             <form action="index.php?action=updateFestival" method = "POST">
+                                
+                                <input type="hidden" name="anneeFestival" value="{$annee}" />
+                                
+                                <input type="hidden" name="date" value="{$date}" />
+                                <input type="hidden" name="nbtables" value="{$nbtables}" />
+                                <input type="hidden" name="prixplacestd" value="{$prixplacestd}" />
+                                
+                                <br>
+                               <th class="text-center" > <button type="submit" class="btn btn-primary">Modifier</button></th>   
+                            </form>
+                               
+                       
+                       
+EOF;
+    }
+    echo <<< EOF
+        </tr>
+      </tbody>
+                         
+              
+                      
+EOF;
+}
+echo "</div>";
+echo "</div>";
