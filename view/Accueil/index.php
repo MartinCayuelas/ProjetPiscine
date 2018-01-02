bbfe24f5c1976a005db4ba653fbbfd6e56e93bb9
marieSalelles <mariesalelles@gmail.com>
2018-01-02 11:36:36 +0100

    Add files via upload

---
+++ view/Accueil/index.php
@@ -0,0 +1,147 @@
+
+
+<div class="card mb-3 v">
+          <canvas id="myAreaChart" width="0%" height="0"></canvas>
+      </div>
+
+    
+
+            <!-- infos importantes-->
+          <div class="card mb-3">
+            <div class="card-header">
+              <i class="fa fa-bell-o"></i> Données inportantes</div>
+            <div class="list-group list-group-flush small">
+              <a class="list-group-item list-group-item-action" href="#">
+                <div class="espace">
+                  <div class="media-body">
+                   <span class="esp"> <strong>Tables disponibles : </strong></span>
+                    <div class="text-muted smaller"></div>
+                  </div>
+                </div>
+              </a>
+              <a class="list-group-item list-group-item-action" href="#">
+                <div class="espace">
+                  <div class="media-body">
+                   <span class="esp"> <strong> Nombre de jeux inscrits :</strong></span>
+                    <div class="text-muted smaller"></div>
+                  </div>
+                </div>
+              </a>
+              <a class="list-group-item list-group-item-action" href="#">
+                <div class="espace">
+                  <div class="media-body">
+                    <span class="esp"><strong>Tables réservées :</strong></span>
+                    <div class="text-muted smaller"><?php echo $tr ?></div>
+                  </div>
+                </div>
+              </a>
+              <a class="list-group-item list-group-item-action" href="#">
+                <div class="espace">
+                  <div class="media-body">
+                    <span class="esp"><strong>Nombre d'éditeurs présents :</strong></span>
+                    <div class="text-muted smaller"><?php echo $nbE ?></div>
+                  </div>
+                </div>
+              </a>
+            </div>
+        </div>
+
+            <!-- paiement a venir-->
+          <div class="mb-0 mt-4">
+          <hr class="mt-2">
+          <div class="card-columns">
+        
+          <div class="card mb-3">
+            <div class="card-header">
+              <i class="fa fa-bell-o"></i> Paiement à venir</div>
+            <div class="list-group list-group-flush small">
+              <a class="list-group-item list-group-item-action" href="#">
+                <div class="media">
+                  <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/45x45" alt="">
+                  <div class="media-body">
+                    <strong>Editeur 1 </strong>
+                    <div class="text-muted smaller">Somme : 50€</div>
+                  </div>
+                </div>
+              </a>
+              <a class="list-group-item list-group-item-action" href="#">
+                <div class="media">
+                  <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/45x45" alt="">
+                  <div class="media-body">
+                    <strong>Editeur 2</strong>
+                    <div class="text-muted smaller">Somme : 50€</div>
+                  </div>
+                </div>
+              </a>
+              <a class="list-group-item list-group-item-action" href="#">
+                <div class="media">
+                  <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/45x45" alt="">
+                  <div class="media-body">
+                    <strong>Editeur 3</strong>
+                    <div class="text-muted smaller">Somme : 50€</div>
+                  </div>
+                </div>
+              </a>
+              <a class="list-group-item list-group-item-action" href="#">
+                <div class="media">
+                  <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/45x45" alt="">
+                  <div class="media-body">
+                    <strong>Editeur 4</strong>
+                    <div class="text-muted smaller">Somme : 50€</div>
+                  </div>
+                </div>
+              </a>
+            </div>
+        </div>
+
+            <!-- Example Social Card-->
+            <div class="card mb-3 f"> 
+            </div>
+         
+          <!-- jeux a recevoir-->
+          <div class="card mb-3">
+            <div class="card-header">
+              <i class="fa fa-bell-o"></i> Jeux à recevoir</div>
+            <div class="list-group list-group-flush small">
+              <a class="list-group-item list-group-item-action" href="#">
+                <div class="media">
+                  <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/45x45" alt="">
+                  <div class="media-body">
+                    <strong>Editeur 1</strong> 
+                    <div class="text-muted smaller">Jeux : date</div>
+                  </div>
+                </div>
+              </a>
+              <a class="list-group-item list-group-item-action" href="#">
+                <div class="media">
+                  <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/45x45" alt="">
+                  <div class="media-body">
+                    <strong>Editeur 2</strong>
+                    <div class="text-muted smaller">Jeux : date</div>
+                  </div>
+                </div>
+              </a>
+              <a class="list-group-item list-group-item-action" href="#">
+                <div class="media">
+                  <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/45x45" alt="">
+                  <div class="media-body">
+                    <strong>Editeur 3</strong>
+                    <div class="text-muted smaller">Jeux : date</div>
+                  </div>
+                </div>
+              </a>
+              <a class="list-group-item list-group-item-action" href="#">
+                <div class="media">
+                  <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/45x45" alt="">
+                  <div class="media-body">
+                    <strong>Editeur 4 </strong>.
+                    <div class="text-muted smaller">Jeux : date</div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+//EOF;
