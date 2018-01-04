66b9c290a855f3aeaf0b7a2b7c50f3d63f9a0296
marieSalelles <mariesalelles@gmail.com>
2018-01-04 11:56:24 +0100

    Add files via upload

---
+++ view/Accueil/indexVide.php
@@ -0,0 +1,71 @@
+<div class="card mb-3 v">
+          <canvas id="myAreaChart" width="0%" height="0"></canvas>
+      </div>
+
+            <!-- infos importantes-->
+          <div class="card mb-3">
+            <div class="card-header">
+              <i class="fa fa-bell-o"></i> Données inportantes</div>
+            <div class="list-group list-group-flush small">
+              <a class="list-group-item list-group-item-action" href="#">
+                <div class="espace">
+                  <div class="media-body">
+                   <span class="esp"> <strong>Tables disponibles : </strong></span>
+                    <div class="text-muted smaller"><?php echo $tD2 ?></div>
+                  </div>
+                </div>
+              </a>
+              <a class="list-group-item list-group-item-action" href="#">
+                <div class="espace">
+                  <div class="media-body">
+                   <span class="esp"> <strong> Nombre de jeux :</strong></span>
+                    <div class="text-muted smaller"><?php echo $nbJ ?></div>
+                  </div>
+                </div>
+              </a>
+              <a class="list-group-item list-group-item-action" href="#">
+                <div class="espace">
+                  <div class="media-body">
+                    <span class="esp"><strong>Tables réservées :</strong></span>
+                    <div class="text-muted smaller"><?php echo $tr ?></div>
+                  </div>
+                </div>
+              </a>
+              <a class="list-group-item list-group-item-action" href="#">
+                <div class="espace">
+                  <div class="media-body">
+                    <span class="esp"><strong>Nombre d'éditeurs :</strong></span>
+                    <div class="text-muted smaller"><?php echo $nbE ?></div>
+                  </div>
+                </div>
+              </a>
+            </div>
+        </div>
+
+            <!-- paiement a venir-->
+          <div class="mb-0 mt-4">
+          <hr class="mt-2">
+          <div class="card-columns">
+        
+          <div class="card mb-3">
+            <div class="card-header">
+              <i class="fa fa-bell-o"></i> Paiement à venir</div>
+
+
+      </div>
+      </div>
+      </div>
+
+            <!-- Example Social Card-->
+            <div class="card mb-3 f"> 
+            </div>
+            <!-- jeux a recevoir-->
+          <div class="card mb-3">
+            <div class="card-header">
+              <i class="fa fa-bell-o"></i> Jeux à recevoir</div>
+                
+           
+      </div>
+    </div>
+  </div>
+</div>
