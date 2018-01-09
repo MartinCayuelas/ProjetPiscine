2c7df045942c678eee3fc33887752f2558a83e8f
NicolasG07 <32355895+NicolasG07@users.noreply.github.com>
2018-01-09 14:43:43 +0100

    tri ville

--- model/ModelEditeur.php
+++ model/ModelEditeur.php
@@ -58,7 +58,7 @@ class ModelEditeur {
     public function getAllEditeursSort(){
         $sql = "SELECT * 
                 FROM editeur
-                ORDER BY nomEditeur ASC ";
+                ORDER BY villeEditeur ASC ";
                 $req = Model::$pdo->query($sql);
         $tab_prod = $req->FETCHALL(PDO::FETCH_CLASS, 'ModelEditeur');
 
@@ -68,6 +68,18 @@ class ModelEditeur {
         }
         return $tab_prod;
 
+    public function getAllEditeursSortVille(){
+        $sql = "SELECT * 
+                FROM editeur
+                ORDER BY nomEditeur ASC ";
+                $req = Model::$pdo->query($sql);
+        $tab_prod = $req->FETCHALL(PDO::FETCH_CLASS, 'ModelEditeur');
+
+
+        if (empty($tab_prod)) {
+            return false;
+        }
+        return $tab_prod;
 
     }
     public function getNbEditeurs() {
