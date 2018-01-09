ca63a3eda8181c883b2526e9a31910abdbc762cd
NicolasG07 <32355895+NicolasG07@users.noreply.github.com>
2018-01-09 17:23:26 +0100

    ajout zones

--- view/view.php
+++ view/view.php
@@ -47,7 +47,7 @@
                         </a>
                     </li>
                     <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Categories">
-                        <a class="nav-link" href="Zone.html">
+                        <a class="nav-link" href="index.php?action=listZone">
                             <i class="fa fa-plus-circle" aria-hidden="true"></i>
                             <span class="nav-link-text">Zones</span>
                         </a>
