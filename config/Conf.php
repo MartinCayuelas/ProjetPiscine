a0f30001950e09f7dd9318e372adde0e5877f6d0
MartinCayuelas <martincayuelas@gmail.com>
2018-01-09 13:56:34 +0100

    Update Conf.php

--- config/Conf.php
+++ config/Conf.php
@@ -4,16 +4,12 @@ class Conf {
    
   static private $databases = array(
    
-    // ou localhost sur votre machine
     'hostname' => 'localhost',
-    // A l'IUT, vous avez une BDD nommee comme votre login
-    // Sur votre machine, vous devrez creer une BDD
+    
     'database' => 'festival',
-    // A l'IUT, c'est votre login
-    // Sur votre machine, vous avez surement un compte 'root'
+   
     'login' => 'root',
-    // A l'IUT, c'est votre mdp (INE par defaut)
-    // Sur votre machine personelle, vous avez creez ce mdp a l'installation
+    
     'password' => '*******'
   );
    
