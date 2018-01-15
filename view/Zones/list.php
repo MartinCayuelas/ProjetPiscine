021eb4fc8585e2cf929f6ced4fdbc1f9aef49c1f
marieSalelles <mariesalelles@gmail.com>
2018-01-15 19:53:44 +0100

    Update list.php

--- view/Zones/list.php
+++ view/Zones/list.php
@@ -16,12 +16,13 @@ echo <<<EOF
                   {$num} Zone{$s} <i class=" fa fa-window-maximize"></i>
         </div>
 EOF;
-
+//on recupere les infos des zones
 foreach ($tabZ as $k) {
     $num= htmlspecialchars($k->getNumZone());
     $nom = htmlspecialchars($k->getNomZone());
 
     $nbJ=0;
+ //on cherche le nombre de jeu dans la zone recuperee precedemment
     foreach($tabLoc as $lo) {
       if ($lo->getNumZone() == $num){
         $nbJ = $nbJ+1;
@@ -64,25 +65,26 @@ EOF;
                 
 
 EOF;
+ //on recupere les infos de la table organiser
     foreach ($tabC as $c) {
         if ($c->getNumZone() == $num) {
             $numZ=htmlspecialchars($c->getNumZone());
             $numC = htmlspecialchars($c->getCodeCategorie());
-
+//on recupere le nom de la categorie avec le code 
          foreach ($tabCat as $cat) {
               if ($cat->getCodeCategorie() == $numC){
                   $nomCa=  htmlspecialchars($cat->getNomCategorie());
-
+//on recupere les infos d'un jeu avec la categorie precedemment trouvee
             foreach ($tabJ as $j) {
               if ($j->getcodeCategorie() == $numC) {
 
               $nomJeu = htmlspecialchars($j->getNomJeu());
               $numJeu =  htmlspecialchars($j->getnumJeu());
-
+//on cherche le num resa en fonction du num jeu trouve
               foreach ($tabConcern as $co) {
                if ($co->getNumJeux() == $numJeu) {
                 $numRes=  htmlspecialchars($co->getNumResa());
-
+//on affiche si cela correspond a la resa trouvée precedemment et si numZone de $tabLocegal zone de départ 
                 foreach ($tabLoc as $lo) {
                     if ($lo->getNumResa() == $numRes) {
                     $numZone=  htmlspecialchars($lo->getNumZone());
